020e0c32ffe9f1356dfcf3bf3f2990420e5bae99
Ram <sriramkumar.gb@gmail.com>
2022-06-05 15:53:33 -0700

    Add a bg + heading

--- n-queens/react-based/src/App.js
+++ n-queens/react-based/src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+
+/*
+const rectInfos = [
+  [{top:"0px",left:"0px",color:"white"},{top:"0px",left:"50px",color:"black"},
+    {top:"0px",left:"100px",color:"white"},{top:"0px",left:"150px",color:"black"},
+    {top:"0px",left:"200px",color:"white"},{top:"0px",left:"250px",color:"black"},
+    {top:"0px",left:"300px",color:"white"},{top:"0px",left:"350px",color:"black"}],
+  [{top:"50px",left:"0px",color:"black"},{top:"50px",left:"50px",color:"white"},
+    {top:"50px",left:"100px",color:"black"},{top:"50px",left:"150px",color:"white"},
+    {top:"50px",left:"200px",color:"black"},{top:"50px",left:"250px",color:"white"},
+    {top:"50px",left:"300px",color:"black"},{top:"50px",left:"350px",color:"white"}],
+  [{top:"100px",left:"0px",color:"white"},{top:"100px",left:"50px",color:"black"},
+    {top:"100px",left:"100px",color:"white"},{top:"100px",left:"150px",color:"black"},
+    {top:"100px",left:"200px",color:"white"},{top:"100px",left:"250px",color:"black"},
+    {top:"100px",left:"300px",color:"white"},{top:"100px",left:"350px",color:"black"}],
+  [{top:"150px",left:"0px",color:"black"},{top:"150px",left:"50px",color:"white"},
+    {top:"150px",left:"100px",color:"black"},{top:"150px",left:"150px",color:"white"},
+    {top:"150px",left:"200px",color:"black"},{top:"150px",left:"250px",color:"white"},
+    {top:"150px",left:"300px",color:"black"},{top:"150px",left:"350px",color:"white"}],
+]
+
+const width = 50
+
+const rects = rectInfos.map((row,i)=>
+   row.map((svgElement,j)=>
+       <svg key={`i+${i}+${j}+j`} style={{width:"50px", height:"50px",position: "absolute", top:svgElement.top, left:svgElement.left}}>
+           <rect style={{fill:svgElement.color, width:"50px", height:"50px"}} />
+       </svg>
+   )
+)
+
+        <div style={{position: "relative", width:"1000px", height:"300px"}}>
+          {rects}
+        </div>
+*/
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1> N - Queens (React-based) </h1>
       </header>
     </div>
   );
