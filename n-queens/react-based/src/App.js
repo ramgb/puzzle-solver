9338cd57485a32fc16ad47ee5c9a6340c728d089
Ram <sriramkumar.gb@gmail.com>
2022-06-09 05:43:04 -0700

    Input value useState, useRef

--- n-queens/react-based/src/App.js
+++ n-queens/react-based/src/App.js
@@ -1,11 +1,35 @@
+import React, { useRef, useState } from 'react'
+import NQueensVisualizer from './NQueensVisualizer';
+
 function App() {
+  const numSquaresRef = useRef()
+  const [numSquares, setNumSquares] = useState(8);
+
+  function handleRenderBoard(e) {
+    const numSquaresCurValue = numSquaresRef.current.value
+    if (Number(numSquaresCurValue)) {
+      setNumSquares(numSquaresCurValue)
+    } else {
+      alert("Please enter a valid number")
+    }
+    numSquaresRef.current.value = null
+  }
+
+  function handleIntegerValue(e) {
+    if (!Number(e.target.value)) {
+      alert("Only numbers are valid")
+      e.target.value = null
+    }
+  }
+
   return (
-    <div className="App">
-      <h1> N - Queens Visualizer </h1>
-      <label> Enter number of queens : </label>
-      <input type="text" />
-      <button> Render Board </button>
-    </div>
+    <>
+        <h1> N-Queens Visualizer</h1>
+        <label> Enter number of queens: </label>
+        <input ref={numSquaresRef} type="text" onChange={handleIntegerValue} />
+        <button onClick={handleRenderBoard}> Render Board </button>
+        <NQueensVisualizer numSquares={numSquares}/>
+    </>
   );
 }
 
---
+++ n-queens/react-based/src/NQueensVisualizer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class NQueensVisualizer extends Component {
+  render() {
+    return (
+      <div>N-Queens Visualizer {this.props.numSquares} </div>
+    )
+  }
+}
