604de9ae49405bcf7d894bac7349e47ebb4589d0
Ram <sriramkumar.gb@gmail.com>
2022-06-09 05:47:38 -0700

    Some basic todos for rendering

--- n-queens/react-based/src/NQueensVisualizer.js
+++ n-queens/react-based/src/NQueensVisualizer.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 
+// 1. Render the board with number of squares
+// 2. Create a state variable with set of queens placed
+// 3. Allow queens to be placed in each square
+// 4. Re-render when each queen is placed
+ 
 export default class NQueensVisualizer extends Component {
   render() {
     return (
